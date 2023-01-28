9909771772d940b9877454979e392e83c39bd82f
zamotaev.v-a@yandex.ru <zamotaev.v-a@yandex.ru>
2023-01-28 05:34:15 +0300

    Updated core admin.py

--- src/core/admin.py
+++ src/core/admin.py
@@ -6,21 +6,12 @@ from core.models import User
 
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
-    list_display = ('username', 'email', 'first_name', 'last_name')
-    search_fields = ('username', 'email', 'first_name', 'last_name')
-    readonly_fields = ('last_login', 'date_joined')
+    list_display = ("username", "email", "first_name", "last_name")
+    search_fields = ("username", "email", "first_name", "last_name")
+    readonly_fields = ("last_login", "date_joined")
     fieldsets = (
-        (None, {'fields': ('username', 'password')}),
-        (
-            'Personal Info',
-            {'fields': ('first_name', 'last_name', 'email')}
-        ),
-        (
-            'Permission',
-            {'fields': ('is_active', 'is_staff', 'is_superuser')}
-        ),
-        (
-            'Important dates',
-            {'fields': ('last_login', 'date_joined')}
-        )
+        (None, {"fields": ("username", "password")}),
+        ("Personal Info", {"fields": ("first_name", "last_name", "email")}),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser")}),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
     )
