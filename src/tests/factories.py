d7791e38ed330e9c6e7c03df7c7efa28c5a90cd9
mari <altysheva_maria@mail.ru>
2023-02-11 09:38:55 +0300

    tests

---
+++ src/tests/factories.py
@@ -0,0 +1,65 @@
+import factory
+
+from core.models import User
+from goals.models import (Board, BoardParticipant, Goal, GoalCategory,
+                          GoalComment)
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+    username = factory.Faker('name')
+    first_name = factory.Faker('first_name')
+    last_name = factory.Faker('last_name')
+    email = factory.Faker('email')
+    password = factory.Faker('password')
+
+    class Meta:
+        model = User
+
+
+class BoardFactory(factory.django.DjangoModelFactory):
+    title = factory.Faker('sentence', nb_words=2)
+    is_deleted = False
+
+    class Meta:
+        model = Board
+
+
+class BoardParticipantFactory(factory.django.DjangoModelFactory):
+    board = factory.SubFactory(BoardFactory)
+    user = factory.SubFactory(UserFactory)
+    role = 1
+
+    class Meta:
+        model = BoardParticipant
+
+
+class GoalCategoryFactory(factory.django.DjangoModelFactory):
+    title = factory.Faker('sentence', nb_words=2)
+    is_deleted = False
+    user = factory.SubFactory(UserFactory)
+    board = factory.SubFactory(BoardFactory)
+
+    class Meta:
+        model = GoalCategory
+
+
+class GoalFactory(factory.django.DjangoModelFactory):
+    title = factory.Faker('sentence', nb_words=2)
+    description = factory.Faker('text')
+    user = factory.SubFactory(UserFactory)
+    category = factory.SubFactory(GoalCategoryFactory)
+    status = 1
+    priority = 2
+    due_date = None
+
+    class Meta:
+        model = Goal
+
+
+class GoalCommentFactory(factory.django.DjangoModelFactory):
+    text = factory.Faker('text')
+    goal = factory.SubFactory(GoalFactory)
+    user = factory.SubFactory(UserFactory)
+
+    class Meta:
+        model = GoalComment
---
+++ src/tests/goals/board_test.py
@@ -0,0 +1,90 @@
+import pytest
+
+from goals.serializers import BoardListSerializer, BoardSerializer
+from tests.factories import BoardFactory, BoardParticipantFactory
+
+
+@pytest.mark.django_db
+def test_board_create(client, get_credentials, user):
+    """
+    Test board creating
+    :param client: Django test client
+    :param get_credentials: Function to get user token for auth
+    :param user: User instance
+    :return: None
+    """
+    """create board test"""
+    data = {
+        'title': 'title',
+        'user': user.id,
+    }
+
+    response = client.post(
+        path='/goals/board/create',
+        HTTP_AUTHORIZATION=get_credentials,
+        data=data,
+        content_type='application/json'
+    )
+
+    assert response.status_code == 201
+    assert response.data['title'] == data['title']
+
+
+@pytest.mark.django_db
+def test_board_list(client, get_credentials, board_participant):
+    """
+    Test board list view
+    :param client: Django test client
+    :param get_credentials: Function to get user token for auth
+    :param board_participant: Board participant
+    :return: None
+    """
+    boards = [board_participant.board]
+    boards.extend(BoardFactory.create_batch(5))
+    for board in boards[1:]:
+        BoardParticipantFactory.create(user=board_participant.user, board=board)
+    boards.sort(key=lambda x: x.id)
+
+    response = client.get(
+        path='/goals/board/list',
+        HTTP_AUTHORIZATION=get_credentials
+    )
+
+    assert response.status_code == 200
+    assert response.data == BoardListSerializer(boards, many=True).data
+
+
+@pytest.mark.django_db
+def test_board_retrieve(client, get_credentials, board, board_participant):
+    """
+    Test board detail view
+    :param client: Django test client
+    :param get_credentials: Function to get user token for auth
+    :param board_participant: Board participant
+    :return: None
+    """
+    response = client.get(
+        path=f'/goals/board/{board.id}',
+        HTTP_AUTHORIZATION=get_credentials
+    )
+
+    assert response.status_code == 200
+    assert response.data == BoardSerializer(board).data
+
+
+@pytest.mark.django_db
+def test_board_delete(client, get_credentials, board, board_participant):
+    """
+    Test board delete view
+    :param client: Django test client
+    :param get_credentials: Function to get user token for auth
+    :param board_participant: Board participant
+    :return: None
+    """
+    response = client.delete(
+        path=f'/goals/board/{board.id}',
+        HTTP_AUTHORIZATION=get_credentials,
+    )
+
+    assert response.status_code == 204
+    assert response.data is None
